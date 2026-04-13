6a0626c747c5c97a2e7f8095c07e12452ef98d3f
Daniel Monfil <davilesmonfil34@gmail.com>
2026-04-13 12:47:31 -0600

    update cors domain

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
  * ========================= */
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://acmar-frontend.vercel.app',
+  'https://accelmar-inventario.vercel.app',
 ];
 
 app.use(
